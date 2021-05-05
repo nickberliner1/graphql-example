348f2ea18c61ae3685220b879cfdf128872cfd0d
Nick Berliner <40582488+nickberliner1@users.noreply.github.com>
2021-05-05 11:45:33 +0100

    schema working

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require('express');
-const expressGraphQL = require('express-graphql');
+const expressGraphQL = require('express-graphql').graphqlHTTP;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString
+} = require('graphql');
 
 const app = express();
 
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'hello',
+    fields: () => ({
+      message: { 
+        type: GraphQLString,
+        resolve: () => 'hello'
+      }
+    })
+  })
+})
+
 app.use('/graphql', expressGraphQL({
+  schema: schema,
   graphiql: true
 }));
-app.listen(5000., () => console.log('Server Running'));
+app.listen(5000, () => console.log('Server Running'));
